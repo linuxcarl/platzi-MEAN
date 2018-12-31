e308f2d5f00adf4fd585e9d9db78e6a82ad67d53
Carlos Ramírez <carlos@extrasistemas.com>
2018-12-31 02:23:30 -0600

    se cargaron los packetes necesarios para iniciar la aplicacion web con MEAN

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule } from "./material.module";
 import { AppComponent } from './app.component';
-
+import { from } from 'rxjs';
+import 'hammerjs';
+import { QuestionDetailComponent } from './questions/question-detail/question-detail.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
